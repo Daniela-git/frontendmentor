4471cde324adf1b8352ba103807699f333434e95
Daniela <dhiguitaa17@gmail.com>
2020-12-18 16:05:00 -0500

    mejora en la vista de la lista

--- faq-accordion-card-main/app.js
+++ faq-accordion-card-main/app.js
@@ -7,6 +7,7 @@ questions.forEach((question) => question.addEventListener("click", showAns));
 //function
 function showAns(e) {
 	let question_title = e.target.parentNode;
+	question_title.children[0].classList.toggle("active");
 	question_title.children[1].classList.toggle("unactive");
 	question_title.children[2].classList.toggle("unactive");
   let question = question_title.parentNode;
