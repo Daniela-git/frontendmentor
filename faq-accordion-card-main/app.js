4a34ade8aae7a8030619da39887a9a766ddad343
Daniela <dhiguitaa17@gmail.com>
2020-12-18 15:32:15 -0500

    show text with click action

---
+++ faq-accordion-card-main/app.js
@@ -0,0 +1,14 @@
+//values
+let questions = document.querySelectorAll(".question");
+
+//event listeners
+questions.forEach((question) => question.addEventListener("click", showAns));
+
+//function
+function showAns(e) {
+	let question_title = e.target.parentNode;
+	question_title.children[1].classList.toggle("unactive");
+	question_title.children[2].classList.toggle("unactive");
+  let question = question_title.parentNode;
+  question.children[1].classList.toggle("active")
+}
