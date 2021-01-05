84c805e753543f6ed55548a390577cfc61dd6aad
Daniela <dhiguitaa17@gmail.com>
2021-01-05 12:09:54 -0500

    funciona el mostrar la respuesta

--- faq-accordion-card-main/app.js
+++ faq-accordion-card-main/app.js
@@ -2,14 +2,16 @@
 let questions = document.querySelectorAll(".question");
 
 //event listeners
-questions.forEach((question) => question.addEventListener("click", showAns));
+questions.forEach(function (question){
+	const title = question.querySelector("h2")
+	title.addEventListener('click',showAns)
+});
 
 //function
 function showAns(e) {
-	let question_title = e.target.parentNode;
-	question_title.children[0].classList.toggle("active");
-	question_title.children[1].classList.toggle("unactive");
-	question_title.children[2].classList.toggle("unactive");
-  let question = question_title.parentNode;
+	let title = e.target.parentNode;
+	title.children[1].classList.toggle("active");
+	title.children[0].classList.toggle("active");
+  let question = title.parentNode;
   question.children[1].classList.toggle("active")
 }
