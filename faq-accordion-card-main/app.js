444c7b0c55134194dfdc1545bda8498a150efa47
Daniela <dhiguitaa17@gmail.com>
2021-01-06 11:13:49 -0500

    nueva funcionalidad js
    se creo una nueva funcion que permite que al dar clic en otra pregunta si hay otra activa esta se cierre antes de que la nueva se abra

--- faq-accordion-card-main/app.js
+++ faq-accordion-card-main/app.js
@@ -9,9 +9,23 @@ questions.forEach(function (question){
 
 //function
 function showAns(e) {
+	removeActives(e.target)
 	let title = e.target.parentNode;
 	title.children[1].classList.toggle("active");
 	title.children[0].classList.toggle("active");
   let question = title.parentNode;
   question.children[1].classList.toggle("active")
+}
+
+function removeActives(active){
+	questions.forEach((question)=>{
+		const title = question.querySelector("h2")
+		if (active.innerText !== title.innerText) {
+			const img = question.querySelector("svg")
+			const text = question.querySelector("p")
+			title.classList.remove("active")
+			img.classList.remove("active")
+			text.classList.remove("active")
+		}
+	})
 }
