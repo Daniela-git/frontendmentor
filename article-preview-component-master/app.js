90e61e7075f61b2ab154d199c4c4d370f65967be
Daniela <dhiguitaa17@gmail.com>
2021-01-12 11:44:08 -0500

    aparece las redes al dar clic

--- article-preview-component-master/app.js
+++ article-preview-component-master/app.js
@@ -1,6 +1,9 @@
 const share = document.querySelector(".user__share");
+const social = document.querySelector(".social");
 share.addEventListener("click", showSocial);
 
 function showSocial(e) {
 	console.log("social");
+	social.classList.toggle("none");
+	share.classList.toggle("clic");
 }
